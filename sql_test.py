c39054fe53dbfda9b20b00257a87872345ef1450
jy-huzhilin <huzhilin@cui.jyzcgl.com>
2026-03-09 10:35:17 +0800

    fix time format and add time limits to all timed tables

--- sql_test.py
+++ sql_test.py
@@ -1,3 +1,13 @@
+import logging
+from typing import Dict
+from datetime import datetime
+import pandas as pd
+
 class SqlTest:
-    def compute(self, inputs, task):
-        pass
+    def compute(self, input: Dict[str, pd.DataFrame],  current_time: datetime) -> Dict[str, pd.DataFrame]:
+        for key, value in input.items():
+            if value is None:
+                logging.info(f"{key}的数据是空")
+            else:
+                logging.info(f"{key}的数据是{value}")
+        return {}
